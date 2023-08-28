ba96dc514d77668adf9caec659e57d4d985ddc66
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-08-28 21:50:25 +0530

    Create Solution.cpp

---
+++ 1. Two Sum/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numToIndex; 
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                return {numToIndex[complement], i};
+            }
+            numToIndex[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
