2812b8b15d25702f30eb6938c375905c00e32552
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-08-28 22:26:06 +0530

    Create SSolution.java

---
+++ 2. Add Two Numbers/SSolution.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyhead = new ListNode(0);
+        ListNode current = dummyhead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        
+        return dummyhead.next;
+    }
+}
