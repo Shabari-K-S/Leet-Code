7411ccd4084e929719e8cb43c10ea349fade9f31
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-08-29 06:31:50 +0530

    Create Solution.py

---
+++ 6. Zigzag Conversion/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = [''] * numRows 
+        current_row = 0
+        direction = -1
+        
+        for char in s:
+            rows[current_row] += char
+            
+            if current_row == 0 or current_row == numRows - 1:
+                direction *= -1
+            
+            current_row += direction
+            
+        return ''.join(rows) 
