58587ad488ef0b4649136e6404698b238ddeb32e
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2023-08-29 20:44:13 +0530

    Updated Solution.java

--- 12. Integer to Roman/Solution.java
+++ 12. Integer to Roman/Solution.java
@@ -1,31 +1,33 @@
 class Solution {
-    public String intToRoman(int num) {
-        int[] val = {
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4, 1
-        };
-        String[] syms = {
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV", "I"
-        };
-        
-        StringBuilder romanNumeral = new StringBuilder();
+    public int romanToInt(String s) {
+        Map<String, Integer> symbolToValue = new HashMap<>();
+        symbolToValue.put("M", 1000);
+        symbolToValue.put("CM", 900);
+        symbolToValue.put("D", 500);
+        symbolToValue.put("CD", 400);
+        symbolToValue.put("C", 100);
+        symbolToValue.put("XC", 90);
+        symbolToValue.put("L", 50);
+        symbolToValue.put("XL", 40);
+        symbolToValue.put("X", 10);
+        symbolToValue.put("IX", 9);
+        symbolToValue.put("V", 5);
+        symbolToValue.put("IV", 4);
+        symbolToValue.put("I", 1);
+
+        int result = 0;
         int i = 0;
-        
-        while (num > 0) {
-            int div = num / val[i];
-            num %= val[i];
-            
-            while (div > 0) {
-                romanNumeral.append(syms[i]);
-                div--;
+
+        while (i < s.length()) {
+            if (i + 1 < s.length() && symbolToValue.containsKey(s.substring(i, i + 2))) {
+                result += symbolToValue.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                result += symbolToValue.get(s.substring(i, i + 1));
+                i++;
             }
-            
-            i++;
         }
-        
-        return romanNumeral.toString();
+
+        return result;
     }
 }
